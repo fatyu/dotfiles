af72c16a837a741b6b961dc5e6a23b986e329c80
张羽 <zhangyu@utry.cn>
2018-04-20 17:26:02 +0800

    sync data

--- sync-data-github.sh
+++ sync-data-github.sh
@@ -1,3 +1,26 @@
 #/bin/sh
-cp -rf ~/.iterm2/ ~/labs/dotfiles/dotfiles/.iterm2/
+cp -rf ~/.iterm2/ ~/workspace/labs/dotfiles/dotfiles/.iterm2/
+
+cp -rf ~/.oh-my-zsh/ ~/workspace/labs/dotfiles/dotfiles/.oh-my-zsh/
+
+cp -rf ~/.vim/ ~/workspace/labs/dotfiles/dotfiles/.vim/
+
+cp -rf ~/.vimrc ~/workspace/labs/dotfiles/dotfiles/
+
+cp -rf ~/.bash_history ~/workspace/labs/dotfiles/dotfiles/
+
+cp -rf ~/.bash_profile ~/workspace/labs/dotfiles/dotfiles/
+
+cp -rf ~/.bashrc ~/workspace/labs/dotfiles/dotfiles/
+
+cp -rf ~/.gitconfig ~/workspace/labs/dotfiles/dotfiles/
+
+cp -rf ~/.spacevim ~/workspace/labs/dotfiles/dotfiles/
+
+cp -rf ~/.viminfo ~/workspace/labs/dotfiles/dotfiles/
+
+cp -rf ~/.zsh_history ~/workspace/labs/dotfiles/dotfiles/
+
+cp -rf ~/.zshrc ~/workspace/labs/dotfiles/dotfiles/
+
 
