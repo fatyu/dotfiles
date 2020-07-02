3f2534e8eb72d901fe12db1fba04df1e6d04948c
fatyu <33862677@qq.com>
2020-07-02 09:49:29 +0800

    sync data

--- sync.sh
+++ sync.sh
@@ -1,25 +1,25 @@
 #/bin/sh
-cp -rf ~/.iterm2/ ~/workspace/labs/dotfiles/dotfiles/.iterm2/
+cp -rf ~/.iterm2/ ~/workspace/labs/dotfiles/.iterm2/
 
-cp -rf ~/.oh-my-zsh/ ~/workspace/labs/dotfiles/dotfiles/.oh-my-zsh/
+cp -rf ~/.oh-my-zsh/ ~/workspace/labs/dotfiles/.oh-my-zsh/
 
-cp -rf ~/.vim/ ~/workspace/labs/dotfiles/dotfiles/.vim/
+cp -rf ~/.vim/ ~/workspace/labs/dotfiles/.vim/
 
-cp -rf ~/.vimrc ~/workspace/labs/dotfiles/dotfiles/
+cp -rf ~/.vimrc ~/workspace/labs/dotfiles/
 
-cp -rf ~/.bash_history ~/workspace/labs/dotfiles/dotfiles/
+cp -rf ~/.bash_history ~/workspace/labs/dotfiles/
 
-cp -rf ~/.bash_profile ~/workspace/labs/dotfiles/dotfiles/
+cp -rf ~/.bash_profile ~/workspace/labs/dotfiles/
 
-cp -rf ~/.bashrc ~/workspace/labs/dotfiles/dotfiles/
+cp -rf ~/.bashrc ~/workspace/labs/dotfiles/
 
-cp -rf ~/.gitconfig ~/workspace/labs/dotfiles/dotfiles/
+cp -rf ~/.gitconfig ~/workspace/labs/dotfiles/
 
-cp -rf ~/.spacevim ~/workspace/labs/dotfiles/dotfiles/
+cp -rf ~/.spacevim ~/workspace/labs/dotfiles/
 
-cp -rf ~/.viminfo ~/workspace/labs/dotfiles/dotfiles/
+cp -rf ~/.viminfo ~/workspace/labs/dotfiles/
 
-cp -rf ~/.zshrc ~/workspace/labs/dotfiles/dotfiles/
+cp -rf ~/.zshrc ~/workspace/labs/dotfiles/
 
 echo "---------文件拷贝成功....开始进行git提交:^-^----------"
 git add * 
